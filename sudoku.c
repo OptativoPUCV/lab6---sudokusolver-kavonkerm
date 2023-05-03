04ab0a0cc6d780e695e0e0cbd606edd119bd7476
Replit user <>
2023-05-03 23:27:51 +0000

    new try

--- sudoku.c
+++ sudoku.c
@@ -123,6 +123,13 @@ int is_final(Node* n){
   if (!is_valid(n)){
     return 1;
   }
+  for (int i = 0; i < 9; i++) {
+    for (int j = 0; j < 9; j++) {
+      if (n->sudo[i][j] == 0) {
+        return 0;
+      }
+    }
+  }
   return 0;
 }
 
