55340fadb9abddd7dc0797958e8053955ca8261c
Replit user <>
2023-05-03 22:11:48 +0000

    new try

--- sudoku.c
+++ sudoku.c
@@ -57,7 +57,6 @@ List* get_adj_nodes(Node* n){
     for (col = 0; col < 9; col++){
       if (n->sudo[fil][col] == 0){
         Node *adjNode = (Node*) malloc(sizeof(Node));
-        adjNode->sudo = n->sudo;
         adjNode->sudo[fil][col] = nuevoMov;
         pushBack(list,adjNode);
       }
