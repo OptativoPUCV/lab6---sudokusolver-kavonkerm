b1a2525219f2c1bd1ed2cfb8e7b3ea3b10fc708f
Replit user <>
2023-05-03 20:38:18 +0000

    new try

--- sudoku.c
+++ sudoku.c
@@ -53,12 +53,17 @@ List* get_adj_nodes(Node* n){
   List* list = createList();
   int fil,col; 
   char newMov;
-  if (n->sudo[fil][col] == "\0"){
-    Node* inicial = (Node*) malloc(sizeof(Node));
-    inicial->sudo = n->sudo;
-    inicial->sudo[fil][col] = newMov;
-    pushBack(list, newMov);
+  for(fil = 0; fil < 9; fil++){
+    for(col = 0; col < 9; col++){
+      if (n->sudo[fil][col] == "\0"){
+        Node* inicial = (Node*) malloc(sizeof(Node));
+        inicial->sudo = n->sudo;
+        inicial->sudo[fil][col] = newMov;
+        pushBack(list, newMov);
+      }
+    } 
   }
+  
   return list;
 }
 
