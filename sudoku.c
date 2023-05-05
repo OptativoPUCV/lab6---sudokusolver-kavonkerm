8eb5cab65deff00817c21be7a29ee96f1bd7f86f
Replit user <>
2023-05-05 22:37:38 +0000

    new try

--- sudoku.c
+++ sudoku.c
@@ -149,9 +149,8 @@ Node* DFS(Node* initial, int* cont){
       push(stack,aux);
       aux = next(adj);
       (*cont)++;
-      free(first(stack));
     }
-    
+    free(first(stack));
   }
   return NULL;
 }
