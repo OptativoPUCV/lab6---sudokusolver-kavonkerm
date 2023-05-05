a714e07c3d68bb09646ff0bfb6b603bc1582a4ea
Replit user <>
2023-05-05 22:18:31 +0000

    new try

--- sudoku.c
+++ sudoku.c
@@ -106,7 +106,7 @@ List* get_adj_nodes(Node* n){
           }
           if (is_valid(adjNode)){
             pushBack(list, adjNode);
-            //if (get_size(list) == 3) break;
+            if (get_size(list) == 3) break;
           } else{
             free(adjNode);
           }
