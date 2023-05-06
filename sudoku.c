4a42307a527728b4e39a31e9eb32666af2c4f337
Replit user <>
2023-05-06 00:32:14 +0000

    new try

--- sudoku.c
+++ sudoku.c
@@ -123,7 +123,7 @@ int is_final(Node* n){
   for (int i = 0; i < 9; i++) {
     for (int j = 0; j < 9; j++) {
       if (n->sudo[i][j] == 0) {
-        return 0;
+        return 1;
       }
     }
   }
@@ -135,7 +135,7 @@ int is_final(Node* n){
 
 Node* DFS(Node* initial, int* cont){
   Stack* stack = createStack();
-  //push(stack,initial);
+  push(stack,initial);
   //*cont = 0;
   
   while (top(stack) != NULL){
